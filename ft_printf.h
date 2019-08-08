29ce2ddf7fd60cc6ec3be59e426e8353aea8ab13
Mohammad Butt <mbutt@student.42.us.org>
2019-08-07 20:58:30 -0700

    Creating simple printf

--- ft_printf.h
+++ ft_printf.h
@@ -6,7 +6,7 @@
 /*   By: mbutt <marvin@42.fr>                       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/08/07 15:21:17 by mbutt             #+#    #+#             */
-/*   Updated: 2019/08/07 16:00:16 by mbutt            ###   ########.fr       */
+/*   Updated: 2019/08/07 20:58:06 by mbutt            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,8 +16,11 @@
 # include <unistd.h> /*write(2)*/
 # include <stdlib.h> /*malloc(3), free(3), and exit(3)*/
 # include <stdarg.h> /*va_start, va_arg, va_copy, va_end(3)*/
+# include <stdio.h>
 
-void ft_putstr(char *str);
+//void ft_putstr(char *str);
+
+void simple_printf(const char *fmt, ...);
 
 
 
--- ft_putstr.c
+++ ft_putstr.c
@@ -6,12 +6,42 @@
 /*   By: mbutt <marvin@42.fr>                       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/08/07 15:22:35 by mbutt             #+#    #+#             */
-/*   Updated: 2019/08/07 15:23:23 by mbutt            ###   ########.fr       */
+/*   Updated: 2019/08/07 20:58:01 by mbutt            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "ft_printf.h"
 
+
+void simple_printf(const char *fmt, ...)
+{
+	va_list args;
+	va_start(args, fmt);
+
+	while(*fmt != '\0')
+	{
+		if(*fmt == 'd')
+		{
+			int i = va_arg(args, int);
+			printf("%d\n", i);
+		}
+		else if(*fmt == 'c')
+		{
+			int c = va_arg(args, int);
+			printf("%c\n", c);
+		}
+		else if(*fmt == 'f')
+		{
+			double d = va_arg(args, double);
+			printf("%f\n", d);
+		}
+		++fmt;
+	}
+	va_end(args);
+}
+
+
+/*
 void ft_putstr(char *str)
 {
 	int i;
@@ -21,3 +51,4 @@ void ft_putstr(char *str)
 		while(str[i])
 			write(1, &str[i++], 1);
 }
+*/
