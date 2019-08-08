9cba64a2fa734f4ce79c710e96767996ca3ff06e
Mohammad Butt <mbutt@student.42.us.org>
2019-08-07 17:45:37 -0700

    Updating

--- ft_printf.h
+++ ft_printf.h
@@ -6,17 +6,19 @@
 /*   By: mbutt <marvin@42.fr>                       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/08/07 15:21:17 by mbutt             #+#    #+#             */
-/*   Updated: 2019/08/07 15:28:34 by mbutt            ###   ########.fr       */
+/*   Updated: 2019/08/07 16:00:16 by mbutt            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef FT_PRINTF_H
 # define FT_PRINTF_H
 
-# include <stdio.h>
-# include <unistd.h>
+# include <unistd.h> /*write(2)*/
+# include <stdlib.h> /*malloc(3), free(3), and exit(3)*/
+# include <stdarg.h> /*va_start, va_arg, va_copy, va_end(3)*/
 
 void ft_putstr(char *str);
+
 
 
 #endif
