de470e74015784ac22328275b395c4e82c7d2502
Mohammad Butt <mbutt@student.42.us.org>
2019-08-13 20:18:54 -0700

    Added a new way to do ft_strlen

--- mini_libft/ft_strlen.c
+++ mini_libft/ft_strlen.c
@@ -6,12 +6,13 @@
 /*   By: mbutt <marvin@42.fr>                       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/08/11 15:50:22 by mbutt             #+#    #+#             */
-/*   Updated: 2019/08/11 15:51:59 by mbutt            ###   ########.fr       */
+/*   Updated: 2019/08/13 20:18:09 by mbutt            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "mini_libft.h"
-
+#include <stdio.h>
+/*
 int ft_strlen(char *str)
 {
 	int i;
@@ -22,3 +23,29 @@ int ft_strlen(char *str)
 			i++;
 	return (i);
 }
+*/
+
+int ft_strlen(char *str)
+{
+	int i;
+
+	i = 0;
+	if(str == NULL)
+		return(i);
+	if(str[i] != '\0')
+	{
+		i++;
+//		i = 1 + ft_strlen(str + i);
+		i = 1 + ft_strlen(&str[i]);
+	}
+	return(i);
+}
+
+/*
+int main(void)
+{
+	char *str = "Test";
+
+	printf("%d\n", ft_strlen(str));
+}
+*/
