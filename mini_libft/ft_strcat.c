17d00c2dc37f4500b1b0b74017b1f9b749162449
Mohammad Butt <mbutt@student.42.us.org>
2019-09-11 13:19:56 -0700

    Improved ft_strcat and ft_strcat_loop. Need to implement colors

--- mini_libft/ft_strcat.c
+++ mini_libft/ft_strcat.c
@@ -6,13 +6,14 @@
 /*   By: mbutt <marvin@42.fr>                       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/08/26 16:22:56 by mbutt             #+#    #+#             */
-/*   Updated: 2019/09/04 23:24:50 by mbutt            ###   ########.fr       */
+/*   Updated: 2019/09/11 13:19:08 by mbutt            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "mini_libft.h"
 #include <stdio.h>
 
+/*
 char *ft_strcat(char *dest, char *source)
 {
 	int len;
@@ -20,11 +21,48 @@ char *ft_strcat(char *dest, char *source)
 	len = ft_strlen(dest);
 	return(ft_strcpy(dest + len, source));
 }
+*/
 
+
+/*
 char *ft_strcat_loop(char *dest, int x_times, char *source)
 {
 	if(x_times)
 		while(x_times--)
 			ft_strcat(dest, source);
+	return(dest);
+}
+*/
+
+char *ft_strcat(char *dest, char *source)
+{
+	int i;
+	int j;
+
+	i = 0;
+	j = ft_strlen(dest);
+	if(source[i])
+		while(source[i])
+			dest[j++] = source[i++];
+	dest[j] = '\0';
+	return(dest);
+}
+
+char *ft_strcat_loop(char *dest, int x_times, char *source)
+{
+	int i;
+	int j;
+
+	i = 0;
+	j = ft_strlen(dest);
+	if(x_times)
+		while(x_times--)
+		{
+			if(source)
+				while(source[i])
+					dest[j++] = source[i++];
+			i = 0;
+		}
+	dest[j] = '\0';
 	return(dest);
 }
