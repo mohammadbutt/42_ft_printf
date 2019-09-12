bb917d9eceb9c79fe52c6b764180aa1cf6a83644
Mohammad Butt <mbutt@student.42.us.org>
2019-09-12 11:49:30 -0700

    Split type_field_o.c into a seperate file. Everything still passes

--- ft_printf.c
+++ ft_printf.c
@@ -6,7 +6,7 @@
 /*   By: mbutt <marvin@42.fr>                       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/08/08 12:54:07 by mbutt             #+#    #+#             */
-/*   Updated: 2019/09/12 11:33:30 by mbutt            ###   ########.fr       */
+/*   Updated: 2019/09/12 11:49:03 by mbutt            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -282,69 +282,6 @@ char *ft_itoa_min(t_printf *pr, int_fast64_t num, char temp_str[])
 		ft_itoa_min_int(num, temp_str);
 
 	return (temp_str);
-}
-
-void	o_append_buffer(t_printf *pr, char s[], char t_s[])
-{
-	int len;
-
-	len = ft_strlen(t_s);
-
-	if (s[0] == '0')
-		pr->var.precision = ft_pad(pr->precision_field, len + 1);
-	else
-		pr->var.precision = ft_pad(pr->precision_field, len);
-	pr->var.width = ft_pad(pr->width_field, len + pr->var.precision);
-	if (pr->flag.plus == true || pr->flag.space == true || pr->flag.hash == true)
-		if (pr->var.width > 0)	
-			pr->var.width--;
-	if (pr->flag.zero == true && pr->var.width >= 0)
-		while (pr->var.width--)
-			ft_strcat(s, "0");
-	else if (pr->flag.zero == false && pr->flag.minus == false)
-		append_to_buffer_loop(pr, pr->var.width, " ");
-	else if (pr->flag.minus == true)
-		while (pr->var.width--)
-			ft_strcat(t_s, " ");
-	if (pr->var.precision >= 0)
-		while (pr->var.precision--)
-			ft_strcat(s, "0");
-	ft_strcat(s, t_s);
-	append_to_buffer(pr, s);
-}
-
-void check_flags_for_o(t_printf *pr, char s[])
-{
-	if (pr->flag.hash == true)
-		ft_strcpy(s, "0");
-	else if (pr->flag.space == true)
-		ft_strcpy(s, " ");
-}
-
-void collect_o(t_printf *pr)
-{
-	uint_fast64_t n;
-	char s[ft_abs(pr->precision_field) + pr->width_field + 32];
-	char t_s[ft_abs(pr->precision_field) + pr->width_field + 32];
-	
-	ft_bzero(s, ft_abs(pr->precision_field) + pr->width_field + 32);
-	ft_bzero(t_s, ft_abs(pr->precision_field) + pr->width_field + 32);
-	ft_bzero(&pr->var, sizeof(&pr->var));
-	n = 0;
-	n = length_field_uox(pr);
-	check_flags_for_o(pr, s);
-	ft_itoa_base_u(n, FT_OCTAL, t_s);
-	if (t_s[0] == '0' && s[0] == '0' && pr->precision_field == -1)
-		(pr->flag.zero == false) && (ft_strcpy(s, " "));
-	if (s[0] == ' ' && t_s[0] == '0' && pr->flag.minus == true)
-		ft_swap(&s[0], &t_s[0]);
-	else if (n == 0 && pr->width_field == 0 && pr->precision_field == -1)
-		ft_strcpy(s, NULL);
-	if (n == 0 && pr->width_field > 1 && pr->flag.zero == true)
-		(pr->flag.hash == true) && (ft_strcpy(s, "0"));
-	(pr->precision_field != -1) && (pr->flag.zero = false);
-	(pr->precision_field == 0 && n == 0) && (ft_strcpy(t_s, NULL));
-	o_append_buffer(pr, s, t_s);
 }
 
 void check_flags_for_x(t_printf *pr, char s[], uint_fast64_t n)
--- ft_printf.h
+++ ft_printf.h
@@ -6,7 +6,7 @@
 /*   By: mbutt <marvin@42.fr>                       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/08/07 15:21:17 by mbutt             #+#    #+#             */
-/*   Updated: 2019/09/12 11:33:39 by mbutt            ###   ########.fr       */
+/*   Updated: 2019/09/12 11:47:37 by mbutt            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -223,6 +223,14 @@ void			collect_u(t_printf *pr);
 uint_fast64_t	length_field_uox(t_printf *pr);
 void			u_append_buffer(t_printf *pr, char s[], char t_s[]);
 
+/*
+** type_field_o.c
+*/
+
+void	collect_o(t_printf *pr);
+void 	check_flags_for_o(t_printf *pr, char s[]);
+void	o_append_buffer(t_printf *pr, char s[], char t_s[]);
+
 
 
 
@@ -259,10 +267,6 @@ char	*ft_itoa_min_ll(int_fast64_t num, char temp_str[]);
 char	*ft_itoa_min_int(int num, char temp_str[]);
 //void	ft_bzero_buffers(char str[], char temp_str[]);
 //void 	print_d(t_printf *pr);
-//void	print_o(t_printf *pr);
-void	collect_o(t_printf *pr);
-void 	check_flags_for_o(t_printf *pr, char s[]);
-void	o_append_buffer(t_printf *pr, char s[], char t_s[]);
 //void 	print_xX(t_printf *pr);
 void 	collect_x(t_printf *pr);
 /*
