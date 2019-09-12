2f2e7fff86ecc5bafed3cc5c9d62a139d27520cc
Mohammad Butt <mbutt@student.42.us.org>
2019-09-11 22:15:56 -0700

    Split type_field_f.c to a seperate file. Everything passes

--- ft_printf.h
+++ ft_printf.h
@@ -6,7 +6,7 @@
 /*   By: mbutt <marvin@42.fr>                       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/08/07 15:21:17 by mbutt             #+#    #+#             */
-/*   Updated: 2019/09/11 22:05:04 by mbutt            ###   ########.fr       */
+/*   Updated: 2019/09/11 22:15:27 by mbutt            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -166,6 +166,12 @@ void	b_append_buffer(t_printf *pr, char s[], char t_s[]);
 void 	check_flags_for_b(t_printf *pr, char s[]);
 void	collect_b(t_printf *pr);
 
+/*
+** type_field_f.c
+*/
+
+void	collect_f(t_printf *pr);
+void	f_append_buffer(t_printf *pr, char s[], char t_s[]);
 
 
 
@@ -247,8 +253,6 @@ void	x_width_y_precision_y2(t_printf *pr, char str[], char str_hex[]);
 
 
 //void	print_f(t_printf *pr);
-void	collect_f(t_printf *pr);
-void	f_append_buffer(t_printf *pr, char s[], char t_s[]);
 
 //void	print_b(t_printf *pr);
 
